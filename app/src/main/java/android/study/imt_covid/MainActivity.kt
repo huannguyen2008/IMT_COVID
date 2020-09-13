49ca09d654660754e50487a1e2caa78201cb579d
huannguyen2008 <huannguyen2299@gmail.com>
2020-09-13 23:39:49 +0700

    update bottom nav

--- app/src/main/java/android/study/imt_covid/MainActivity.kt
+++ app/src/main/java/android/study/imt_covid/MainActivity.kt
@@ -2,10 +2,37 @@ package android.study.imt_covid
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.study.imt_covid.Fragments.HealthFragment
+import android.study.imt_covid.Fragments.HomeFragment
+import android.study.imt_covid.Fragments.MenuFragment
+import androidx.fragment.app.Fragment
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val homeFrag = HomeFragment()
+        val healthFrag = HealthFragment()
+        val menuFrag = MenuFragment()
+
+        makeCurrentFrag(homeFrag)
+
+        bot_nav.setOnNavigationItemSelectedListener {
+            when(it.itemId){
+                R.id.ic_home -> makeCurrentFrag(homeFrag)
+                R.id.ic_health -> makeCurrentFrag(healthFrag)
+                R.id.ic_menu -> makeCurrentFrag(menuFrag)
+            }
+            true
+        }
     }
+
+    private fun makeCurrentFrag(fragment: Fragment) =
+        supportFragmentManager.beginTransaction().apply {
+            replace(R.id.fl_wrapper,fragment)
+            commit()
+        }
+
 }
